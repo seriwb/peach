77ddccbf8113838dd712e1eff5015ab3aaac1379
seri <seri.wb@gmail.com>
2015-01-18 08:15:21 +0900

    modify config

--- conf/Config.groovy
+++ conf/Config.groovy
@@ -1,5 +1,5 @@
 // リプライ対象とするTweetキーワード（複数指定はカンマ連結で記述する）
-peach.target.tweet = "痩せようaaa,やせようaaa,痩せたいaaa,やせたいaaa"
+peach.target.tweet = "痩せよう"
 //peach.target.tweet = "痩せよう,やせよう,痩せたい,やせたい"
 
 // リプライするメッセージ
